874676496b3e5752dda912877e1b94717efcd59d
Kavindu <kavinduuni@gmail.com>
2026-03-11 05:21:10 +0530

    feat: add initialProjects array and create ProjectCard component

--- projecthub/src/App.jsx
+++ projecthub/src/App.jsx
@@ -1,5 +1,11 @@
 import Navbar from "./components/layout/Navbar.jsx"
 
+const initialProjects = [
+    {id : 1, title : "Project 1", owner : "owner 1"},
+    {id : 2, title : "Project 2", owner : "owner 2"},
+    {id : 3, title : "Project 3", owner : "owner 3"},
+];
+
 function App(){
     return(
         <div>
---
+++ projecthub/src/components/projects/ProjectCard.jsx
@@ -0,0 +1,8 @@
+const ProjectCard = ({title, owner}) =>{
+    return(
+        <div>
+            <h3>{title}</h3>
+            <p>{owner}</p>
+        </div>
+    )
+}
