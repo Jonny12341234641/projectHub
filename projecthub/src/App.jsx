ef6531e97426936c09f128427aeb2ed429aa5e9f
Kavindu <kavinduuni@gmail.com>
2026-03-11 05:53:13 +0530

    feat: add additional projects to initialProjects and enhance ProjectCard click handling

--- projecthub/src/App.jsx
+++ projecthub/src/App.jsx
@@ -5,6 +5,13 @@ const initialProjects = [
     {id : 1, title : "Project 1", owner : "owner 1"},
     {id : 2, title : "Project 2", owner : "owner 2"},
     {id : 3, title : "Project 3", owner : "owner 3"},
+    {id : 4, title : "Project 4", owner : "owner 4"},
+    {id : 5, title : "Project 5", owner : "owner 5"},
+    {id : 6, title : "Project 6", owner : "owner 6"},
+    {id : 7, title : "Project 7", owner : "owner 7"},
+    {id : 8, title : "Project 8", owner : "owner 8"},
+    {id : 9, title : "Project 9", owner : "owner 9"},
+    {id : 10, title : "Project 10", owner : "owner 10"},
 ];
 
 function App(){
--- projecthub/src/components/projects/ProjectCard.jsx
+++ projecthub/src/components/projects/ProjectCard.jsx
@@ -1,8 +1,16 @@
 const ProjectCard = ({title, owner}) =>{
+
+    const handleclick = () => {
+        alert('You clicked on ${title}');
+        console.log('You clicked on ${title}');
+    };
+
     return(
-        <div>
+        <div onClick={handleclick}>
             <h3>{title}</h3>
             <p>{owner}</p>
         </div>
     )
 }
+
+export default ProjectCard;
